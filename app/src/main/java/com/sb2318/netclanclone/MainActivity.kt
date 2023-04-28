fe114709d7f7434caf5dadf504a457aaa4d8c38e
SB2318 <susmitabhattacharya2023@gmail.com>
2023-04-29 00:16:17 +0530

    "Item Layout Done"

--- app/src/main/java/com/sb2318/netclanclone/MainActivity.kt
+++ app/src/main/java/com/sb2318/netclanclone/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sb2318.netclanclone
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.item_home_layout)
+
+        val simpleProgressBar = findViewById<ProgressBar>(R.id.progressBar)
+        simpleProgressBar.progress = 50
     }
 }
